6e51c952953202c5cb52819a48609ca21cfc7854
jefrymld <jefryduvan.maldonado@gmail.com>
2022-10-30 18:14:23 -0500

    ✨feat: login y mensajes de alerta - 2022/30/10

---
+++ modelos/login.php
@@ -0,0 +1,41 @@
+<?php
+
+class login
+{
+
+
+    // Conexión
+    private $conn;
+
+    // Metodos
+    public function __construct()
+    {
+        if (is_file('config/config.php')) {
+            require_once "config/config.php";
+        } else {
+            require_once "../config/config.php";
+        }
+        $this->conn = new Conexion();
+    }
+
+    public function set($atributo, $contenido)
+    {
+        $this->$atributo = $contenido;
+    }
+
+    public function get($atributo)
+    {
+        return $this->$atributo;
+    }
+
+
+    function getUsuarios($user)
+    {
+        $sql = "SELECT * from usuarios WHERE usuario = '$user'";
+        return $this->conn->getData($sql);
+    }
+   
+   
+
+  
+}
